5ffda176e2b1bf358ceb069514965acde7709db2
Jose Mendoza <pball626@gmail.com>
2021-04-15 15:40:00 -0500

    commit tree 86edf746a6e16f871ec1215a20912d0869f78747

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// const titleCased = () => {
+//   return tutorials
+// }
+
+let titleCased = tutorials.map(string.upperCase)
